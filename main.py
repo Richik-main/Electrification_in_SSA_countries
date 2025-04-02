9af00dd2f305135eab8abd5faba516e8ef668eea
Richik Ghosh <143452995+Richik-main@users.noreply.github.com>
2025-04-02 16:11:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from scripts.data_preprocessing import preprocess_data
 from scripts.visualization import generate_all_plots
 from scripts.model_training import train_and_evaluate_models
 from scripts.stats import generate_all_statisticalTestand_plots
-
+from scripts.causal_inferencing import generate_all_casualTest
 def load_config(config_path="config.yaml"):
     """Utility function to load the config file."""
     with open(config_path, "r") as f:
@@ -25,6 +25,8 @@ def main(config_path="config.yaml"):
     #generate_all_plots(df)
 
     generate_all_statisticalTestand_plots(df)
+    # Call the IV regression and related analysis function.
+    generate_all_casualTest(df)
     # 3. Model Training
     best_models, results_df = train_and_evaluate_models(df, config)
 
